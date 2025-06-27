e86103a98b37a56b2f71d5eeefab3580a90ec70e
hack431 <undefeatable126@gmail.com>
2025-06-27 15:07:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "NOVA~TIFE0LpZ#7xQnEqmb3o3iCJ-xo9kE2L6nNVMmLhC8Z8vTZ-5Dl5o",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255767862457",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255677510751",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "nova-xmd",
 // add bot owner name
